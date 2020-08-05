ddb3dd5c4278f4ecfbd76351cb1651581dd9ec49
Jaakko Keränen <jaakko.keranen@iki.fi>
2020-08-05 10:44:52 +0300

    Color: Added HSL conversions
    Removed the extra 15/88 grays from the UI palette.

--- src/ui/color.c
+++ src/ui/color.c
@@ -4,40 +4,125 @@
 
 static const iColor transparent_;
 
+static iColor palette_[max_ColorId] = {
+    { 0,   0,   0,   255 },
+    { 40,  40,  40,  255 },
+    { 80,  80,  80,  255 },
+    { 160, 160, 160, 255 },
+    { 255, 255, 255, 255 },
+    { 106, 80,  0,   255 },
+    { 255, 192, 0,   255 },
+    { 0,   96,  128, 255 },
+    { 0,   192, 255, 255 },
+    { 255, 255, 32,  255 },
+    { 255, 64,  64,  255 },
+    { 255, 0,   255, 255 },
+    { 132, 132, 255, 255 },
+    { 0,   200, 0,   255 },
+    /* theme colors left black until theme is seeded */
+};
+
 iColor get_Color(int color) {
-    static const iColor palette[] = {
-        { 0,   0,   0,   255 },
-        { 24,  24,  24,  255 },
-        { 40,  40,  40,  255 },
-        { 80,  80,  80,  255 },
-        { 160, 160, 160, 255 },
-        { 208, 208, 208, 255 },
-        { 255, 255, 255, 255 },
-        { 106, 80,  0,   255 },
-        { 255, 192, 0,   255 },
-        { 0,   96,  128, 255 },
-        { 0,   192, 255, 255 },
-        { 255, 255, 32,  255 },
-        { 255, 64,  64,  255 },
-        { 255, 0,   255, 255 },
-        { 132, 132, 255, 255 },
-        { 0,   200, 0,   255 },
-    };
-    const iColor *clr = &transparent_;
-    if (color >= 0 && color < (int) iElemCount(palette)) {
-        clr = &palette[color];
+    const iColor *rgba = &transparent_;
+    if (color >= 0 && color < max_ColorId) {
+        rgba = &palette_[color];
     }
-    return *clr;
+    return *rgba;
+}
+
+void set_Color(int color, iColor rgba) {
+    if (color >= tmFirst_ColorId && color < max_ColorId) {
+        palette_[color] = rgba;
+    }
+}
+
+iLocalDef iFloat4 normalize_(iColor d) {
+    return divf_F4(init_F4(d.r, d.g, d.b, d.a), 255.0f);
+}
+
+iLocalDef iColor toColor_(iFloat4 d) {
+    const iFloat4 i = addf_F4(mulf_F4(d, 255.0f), 0.5f);
+    return (iColor){ (uint8_t) x_F4(i),
+                     (uint8_t) y_F4(i),
+                     (uint8_t) z_F4(i),
+                     (uint8_t) w_F4(i) };
+}
+
+iHSLColor hsl_Color(iColor color) {
+    float rgb[4];
+    store_F4(normalize_(color), rgb);
+    int compMax, compMin;
+    if (rgb[0] >= rgb[1] && rgb[0] >= rgb[2]) {
+        compMax = 0;
+    }
+    else if (rgb[1] >= rgb[0] && rgb[1] >= rgb[2]) {
+        compMax = 1;
+    }
+    else {
+        compMax = 2;
+    }
+    if (rgb[0] <= rgb[1] && rgb[0] <= rgb[2]) {
+        compMin = 0;
+    }
+    else if (rgb[1] <= rgb[0] && rgb[1] <= rgb[2]) {
+        compMin = 1;
+    }
+    else {
+        compMin = 2;
+    }
+    const float rgbMax = rgb[compMax];
+    const float rgbMin = rgb[compMin];
+    const float lum = (rgbMax + rgbMin) / 2.0f;
+    float hue = 0.0f;
+    float sat = 0.0f;
+    if (fabsf(rgbMax - rgbMin) > 0.00001f) {
+        float chr = rgbMax - rgbMin;
+        sat = chr / (1.0f - fabsf(2.0f * lum - 1.0f));
+        if (compMax == 0) {
+            hue = (rgb[1] - rgb[2]) / chr + (rgb[1] < rgb[2] ? 6 : 0);
+        }
+        else if (compMax == 1) {
+            hue = (rgb[2] - rgb[0]) / chr + 2;
+        }
+        else {
+            hue = (rgb[0] - rgb[1]) / chr + 4;
+        }
+    }
+    return (iHSLColor){ hue * 60, sat, lum, rgb[3] }; /* hue in degrees */
+}
+
+static float hueToRgb_(float p, float q, float t) {
+    if (t < 0.0f) t += 1.0f;
+    if (t > 1.0f) t -= 1.0f;
+    if (t < 1.0f / 6.0f) return p + (q - p) * 6.0f * t;
+    if (t < 1.0f / 2.0f) return q;
+    if (t < 2.0f / 3.0f) return p + (q - p) * (2.0f / 3.0f - t) * 6.0f;
+    return p;
+}
+
+iColor fromHsl_Color(iHSLColor hsl) {
+    float r, g, b;
+    hsl.hue /= 360.0f;
+    if (hsl.sat < 0.00001f) {
+        r = g = b = hsl.lum;
+    }
+    else {
+        const float q = hsl.lum < 0.5f ? hsl.lum * (1 + hsl.sat)
+                                       : (hsl.lum + hsl.sat - hsl.lum * hsl.sat);
+        const float p = 2 * hsl.lum - q;
+        r = hueToRgb_(p, q, hsl.hue + 1.0f / 3.0f);
+        g = hueToRgb_(p, q, hsl.hue);
+        b = hueToRgb_(p, q, hsl.hue - 1.0f / 3.0f);
+    }
+    return toColor_(init_F4(r, g, b, hsl.a));
 }
 
 const char *escape_Color(int color) {
     static const char *esc[] = {
         black_ColorEscape,
-        gray15_ColorEscape,
         gray25_ColorEscape,
         gray50_ColorEscape,
         gray75_ColorEscape,
-        gray88_ColorEscape,
         white_ColorEscape,
         brown_ColorEscape,
         orange_ColorEscape,
@@ -49,7 +134,7 @@ const char *escape_Color(int color) {
         blue_ColorEscape,
         green_ColorEscape,
     };
-    if (color >= 0 && color < max_ColorId) {
+    if (color >= 0 && color < (int) iElemCount(esc)) {
         return esc[color];
     }
     return white_ColorEscape;
--- src/ui/color.h
+++ src/ui/color.h
@@ -1,15 +1,14 @@
 #pragma once
 
 #include <the_Foundation/range.h>
+#include <the_Foundation/math.h>
 
 enum iColorId {
     none_ColorId = -1,
     black_ColorId,
-    gray15_ColorId,
     gray25_ColorId,
     gray50_ColorId,
     gray75_ColorId,
-    gray88_ColorId,
     white_ColorId,
     brown_ColorId,    
     orange_ColorId,
@@ -20,35 +19,79 @@ enum iColorId {
     magenta_ColorId,
     blue_ColorId,
     green_ColorId,
+    /* content theme colors */
+    tmFirst_ColorId,
+    tmBackground_ColorId = tmFirst_ColorId,
+    tmParagraph_ColorId,
+    tmFirstParagraph_ColorId,
+    tmQuote_ColorId,
+    tmPreformatted_ColorId,
+    tmHeader1_ColorId,
+    tmHeader2_ColorId,
+    tmHeader3_ColorId,
+
+    tmLinkIcon_ColorId,
+    tmLinkIconVisited_ColorId,
+    tmLinkText_ColorId,
+    tmLinkTextHover_ColorId,
+    tmLinkDomain_ColorId,
+    tmLinkLastVisitDate_ColorId,
+
+    tmHypertextLinkIcon_ColorId,
+    tmHypertextLinkIconVisited_ColorId,
+    tmHypertextLinkText_ColorId,
+    tmHypertextLinkTextHover_ColorId,
+    tmHypertextLinkDomain_ColorId,
+    tmHypertextLinkLastVisitDate_ColorId,
+
+    tmGopherLinkIcon_ColorId,
+    tmGopherLinkIconVisited_ColorId,
+    tmGopherLinkText_ColorId,
+    tmGopherLinkTextHover_ColorId,
+    tmGopherLinkDomain_ColorId,
+    tmGopherLinkLastVisitDate_ColorId,
+
+    tmInlineContentMetadata_ColorId,
+    tmBannerBackground_ColorId,
+    tmBannerTitle_ColorId,
+    tmBannerIcon_ColorId,
     max_ColorId
 };
 
-#define mask_ColorId        0x0f
+#define mask_ColorId        0x3f
 #define permanent_ColorId   0x80 /* cannot be changed via escapes */
 
 #define black_ColorEscape   "\r0"
-#define gray15_ColorEscape  "\r1"
-#define gray25_ColorEscape  "\r2"
-#define gray50_ColorEscape  "\r3"
-#define gray75_ColorEscape  "\r4"
-#define gray88_ColorEscape  "\r5"
-#define white_ColorEscape   "\r6"
-#define brown_ColorEscape   "\r7"
-#define orange_ColorEscape  "\r8"
-#define teal_ColorEscape    "\r9"
-#define cyan_ColorEscape    "\r:"
-#define yellow_ColorEscape  "\r;"
-#define red_ColorEscape     "\r<"
-#define magenta_ColorEscape "\r="
-#define blue_ColorEscape    "\r>"
-#define green_ColorEscape   "\r?"
+#define gray25_ColorEscape  "\r1"
+#define gray50_ColorEscape  "\r2"
+#define gray75_ColorEscape  "\r3"
+#define white_ColorEscape   "\r4"
+#define brown_ColorEscape   "\r5"
+#define orange_ColorEscape  "\r6"
+#define teal_ColorEscape    "\r7"
+#define cyan_ColorEscape    "\r8"
+#define yellow_ColorEscape  "\r9"
+#define red_ColorEscape     "\r:"
+#define magenta_ColorEscape "\r;"
+#define blue_ColorEscape    "\r<"
+#define green_ColorEscape   "\r="
 
 iDeclareType(Color)
+iDeclareType(HSLColor)
 
 struct Impl_Color {
     uint8_t r, g, b, a;
 };
 
+struct Impl_HSLColor {
+    float hue, sat, lum, a;
+};
+
 iColor          get_Color       (int color);
+void            set_Color       (int color, iColor rgba);
+
+iHSLColor       hsl_Color       (iColor rgba);
+iColor          fromHsl_Color   (iHSLColor hsl);
+
 iColor          ansi_Color      (iRangecc escapeSequence, int fallback);
 const char *    escape_Color    (int color);
