a0ddbf089bacfe8b9852e77a3bfcfb452d288053
Jaakko Keränen <jaakko.keranen@iki.fi>
2020-08-11 13:24:18 +0300

    SidebarWidget: Adding mode buttons

--- src/ui/sidebarwidget.c
+++ src/ui/sidebarwidget.c
@@ -1,6 +1,8 @@
 #include "sidebarwidget.h"
+#include "labelwidget.h"
 #include "scrollwidget.h"
 #include "paint.h"
+#include "util.h"
 
 struct Impl_SidebarWidget {
     iWidget widget;
@@ -13,11 +15,30 @@ iDefineObjectConstruction(SidebarWidget)
 void init_SidebarWidget(iSidebarWidget *d) {
     iWidget *w = as_Widget(d);
     init_Widget(w);
+    setFlags_Widget(w, resizeChildren_WidgetFlag, iTrue);
     d->mode = documentOutline_SidebarMode;
     addChild_Widget(w, iClob(d->scroll = new_ScrollWidget()));
-    w->rect.size.x = 60 * gap_UI;
+    w->rect.size.x = 80 * gap_UI;
     setFlags_Widget(w, fixedWidth_WidgetFlag, iTrue);
-    setBackgroundColor_Widget(w, red_ColorId);
+    iWidget *modeButtons = makeHDiv_Widget();
+    setFlags_Widget(modeButtons, arrangeWidth_WidgetFlag | arrangeHeight_WidgetFlag, iTrue);
+    addChild_Widget(w, iClob(modeButtons));
+    addChildFlags_Widget(
+        modeButtons,
+        iClob(new_LabelWidget(
+            "\U0001f5b9 Outline", 0, 0, format_CStr("sidebar.mode arg:%d", documentOutline_SidebarMode))), frameless_WidgetFlag);
+    addChildFlags_Widget(
+        modeButtons,
+        iClob(new_LabelWidget(
+            "\U0001f588 Bookmarks", 0, 0, format_CStr("sidebar.mode arg:%d", bookmarks_SidebarMode))), frameless_WidgetFlag);
+    addChildFlags_Widget(
+        modeButtons,
+        iClob(new_LabelWidget(
+            "\U0001f553 History", 0, 0, format_CStr("sidebar.mode arg:%d", history_SidebarMode))), frameless_WidgetFlag);
+    addChildFlags_Widget(
+        modeButtons,
+        iClob(new_LabelWidget(
+            "\U0001f464 Identities", 0, 0, format_CStr("sidebar.mode arg:%d", identities_SidebarMode))), frameless_WidgetFlag);
 }
 
 void deinit_SidebarWidget(iSidebarWidget *d) {
