2f4cc8dc3f76b862d9c17d96561ba54fe77d0199
Jaakko Keränen <jaakko.keranen@iki.fi>
2020-12-01 10:33:45 +0200

    GmDocument: Spacing before/after links
    Respect the source whitespace, but leave half a line at minimum.

--- src/gmdocument.c
+++ src/gmdocument.c
@@ -278,10 +278,10 @@ static void doLayout_GmDocument_(iGmDocument *d) {
         5, 10, 5, 10, 0, 0, 0, 5
     };
     static const float topMargin[max_GmLineType] = {
-        0.0f, 0.333f, 1.0f, 0.5f, 2.0f, 1.5f, 1.0f, 1.0f
+        0.0f, 0.333f, 1.0f, 0.5f, 2.0f, 1.5f, 1.0f, 0.5f
     };
     static const float bottomMargin[max_GmLineType] = {
-        0.0f, 0.333f, 1.0f, 0.5f, 0.5f, 0.5f, 0.5f, 1.0f
+        0.0f, 0.333f, 1.0f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f
     };
     static const char *arrow           = "\u27a4";
     static const char *envelope        = "\U0001f4e7";
