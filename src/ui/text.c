cef2d288555326e4e268577b63407b4290ca2511
Jaakko Keränen <jaakko.keranen@iki.fi>
2020-12-12 18:22:27 +0200

    Text: Smaller monospace body font

--- src/ui/text.c
+++ src/ui/text.c
@@ -239,7 +239,7 @@ static void initFonts_Text_(iText *d) {
         { h12Font,                            textSize * 2.000f,    h123Scaling,  hugeSymbols_FontId },
         { lightFont,                          textSize * 1.666f,    lightScaling, largeSymbols_FontId },
         /* monospace content fonts */
-        { &fontIosevkaTermExtended_Embedded,  textSize,             0.866f, symbols_FontId },
+        { &fontIosevkaTermExtended_Embedded,  textSize,             0.75f, symbols_FontId },
         /* symbol fonts */
         { &fontSymbola_Embedded,              fontSize_UI,          1.0f, defaultSymbols_FontId },
         { &fontSymbola_Embedded,              fontSize_UI * 1.125f, 1.0f, defaultMediumSymbols_FontId },
