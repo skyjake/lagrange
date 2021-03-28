146f4a0fa9e605afba175d2188005789f5575964
Jaakko Keränen <jaakko.keranen@iki.fi>
2021-03-28 22:06:32 +0300

    Cleanup

--- src/ui/documentwidget.c
+++ src/ui/documentwidget.c
@@ -3216,8 +3216,8 @@ static void drawRun_DrawContext_(void *context, const iGmRun *run) {
             }
         }
     }
-    drawRect_Paint(&d->paint, (iRect){ visPos, run->bounds.size }, green_ColorId);
-    drawRect_Paint(&d->paint, (iRect){ visPos, run->visBounds.size }, red_ColorId);
+//    drawRect_Paint(&d->paint, (iRect){ visPos, run->bounds.size }, green_ColorId);
+//    drawRect_Paint(&d->paint, (iRect){ visPos, run->visBounds.size }, red_ColorId);
 }
 
 static int drawSideRect_(iPaint *p, iRect rect) {
