28ab71b8533afea235ade9e746ebf380f1d92180
Jaakko Keränen <jaakko.keranen@iki.fi>
2021-07-31 08:21:07 +0300

    Window: Fonts reinit after render device/targets reset

--- src/ui/window.c
+++ src/ui/window.c
@@ -816,7 +816,7 @@ iBool processEvent_Window(iWindow *d, const SDL_Event *ev) {
         }
         case SDL_RENDER_TARGETS_RESET:
         case SDL_RENDER_DEVICE_RESET: {
-            invalidate_Window(d);
+            invalidate_Window_(d, iTrue /* force full reset */);
             break;
         }
         default: {
