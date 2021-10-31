511fadcf0aff03d7d6e963143028f0413132a358
Jaakko Keränen <jaakko.keranen@iki.fi>
2021-10-31 05:51:38 +0200

    CI: Install `zip` in Debian environment

--- .github/workflows/docker-debian/entrypoint.sh
+++ .github/workflows/docker-debian/entrypoint.sh
@@ -2,7 +2,7 @@
 export LC_ALL=en_US.UTF-8
 
 apt-get update -qq -y
-apt-get install -y -qq --no-install-recommends cmake libsdl2-dev libssl-dev libpcre3-dev zlib1g-dev libunistring-dev libmpg123-dev debhelper dh-make devscripts fakeroot git build-essential locales python3 python3-pip libharfbuzz-dev libfribidi-dev
+apt-get install -y -qq --no-install-recommends cmake libsdl2-dev libssl-dev libpcre3-dev zlib1g-dev libunistring-dev libmpg123-dev debhelper dh-make devscripts fakeroot git build-essential locales python3 python3-pip zip libharfbuzz-dev libfribidi-dev
 pip3 install git-archive-all
 sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen && locale-gen
 
