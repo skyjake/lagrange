d4a4602a7a018fe5983ade894a0904da82c4e40d
Jaakko Keränen <jaakko.keranen@iki.fi>
2021-11-06 12:29:13 +0200

    Depends: Require the_Foundation v1.0.1

--- Depends.cmake
+++ Depends.cmake
@@ -7,8 +7,41 @@ find_package (PkgConfig)
 find_program (MESON_EXECUTABLE meson DOC "Meson build system")
 find_program (NINJA_EXECUTABLE ninja DOC "Ninja build tool")
 include (ExternalProject)
-
 set (_dependsToBuild)
+
+if (NOT EXISTS ${CMAKE_SOURCE_DIR}/lib/the_Foundation/CMakeLists.txt)
+    set (INSTALL_THE_FOUNDATION YES)
+    find_package (the_Foundation 1.0.1 REQUIRED)
+else ()
+    if (EXISTS ${CMAKE_SOURCE_DIR}/lib/the_Foundation/.git)
+        # the_Foundation is checked out as a submodule, make sure it's up to date.
+        find_package (Git)
+        if (GIT_FOUND)
+            execute_process (
+                COMMAND ${GIT_EXECUTABLE} submodule update
+                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                OUTPUT_VARIABLE subout
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+            )
+            if (subout)
+                message (FATAL_ERROR "'lib/the_Foundation' Git submodule has been updated, please re-run CMake.\n")
+            endif ()
+        endif ()
+    endif ()
+    set (INSTALL_THE_FOUNDATION OFF)
+    set (TFDN_STATIC_LIBRARY    ON  CACHE BOOL "")
+    set (TFDN_ENABLE_INSTALL    OFF CACHE BOOL "")
+    set (TFDN_ENABLE_TESTS      OFF CACHE BOOL "")
+    set (TFDN_ENABLE_WEBREQUEST OFF CACHE BOOL "")
+    add_subdirectory (lib/the_Foundation)
+    add_library (the_Foundation::the_Foundation ALIAS the_Foundation)
+    if (NOT OPENSSL_FOUND)
+        message (FATAL_ERROR "Lagrange requires OpenSSL for TLS. Please check if pkg-config can find 'openssl'.")
+    endif ()
+    if (NOT ZLIB_FOUND)
+        message (FATAL_ERROR "Lagrange requires zlib for reading compressed archives. Please check if pkg-config can find 'zlib'.")
+    endif ()
+endif ()
 
 if (ENABLE_HARFBUZZ AND EXISTS ${CMAKE_SOURCE_DIR}/lib/harfbuzz/CMakeLists.txt)
     # Find HarfBuzz with pkg-config.
@@ -111,40 +144,6 @@ if (ENABLE_FRIBIDI AND EXISTS ${CMAKE_SOURCE_DIR}/lib/fribidi)
 endif ()
 
 add_custom_target (ext-deps DEPENDS ${_dependsToBuild})
-
-if (NOT EXISTS ${CMAKE_SOURCE_DIR}/lib/the_Foundation/CMakeLists.txt)
-    set (INSTALL_THE_FOUNDATION YES)
-    find_package (the_Foundation 1.0.0 REQUIRED)
-else ()
-    if (EXISTS ${CMAKE_SOURCE_DIR}/lib/the_Foundation/.git)
-        # the_Foundation is checked out as a submodule, make sure it's up to date.
-        find_package (Git)
-        if (GIT_FOUND)
-            execute_process (
-                COMMAND ${GIT_EXECUTABLE} submodule update
-                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-                OUTPUT_VARIABLE subout
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-            )
-            if (subout)
-                message (FATAL_ERROR "'lib/the_Foundation' Git submodule has been updated, please re-run CMake.\n")
-            endif ()
-        endif ()
-    endif ()
-    set (INSTALL_THE_FOUNDATION OFF)
-    set (TFDN_STATIC_LIBRARY    ON  CACHE BOOL "")
-    set (TFDN_ENABLE_INSTALL    OFF CACHE BOOL "")
-    set (TFDN_ENABLE_TESTS      OFF CACHE BOOL "")
-    set (TFDN_ENABLE_WEBREQUEST OFF CACHE BOOL "")
-    add_subdirectory (lib/the_Foundation)
-    add_library (the_Foundation::the_Foundation ALIAS the_Foundation)
-    if (NOT OPENSSL_FOUND)
-        message (FATAL_ERROR "Lagrange requires OpenSSL for TLS. Please check if pkg-config can find 'openssl'.")
-    endif ()
-    if (NOT ZLIB_FOUND)
-        message (FATAL_ERROR "Lagrange requires zlib for reading compressed archives. Please check if pkg-config can find 'zlib'.")
-    endif ()
-endif ()
 
 find_package (PkgConfig REQUIRED)
 pkg_check_modules (SDL2 REQUIRED sdl2)
