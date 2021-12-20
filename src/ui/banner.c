3db345897d13df3657e2c6fd90f3bfdeb81df4a0
Jaakko Keränen <jaakko.keranen@iki.fi>
2021-12-20 22:08:13 +0200

    Banner: Margins

--- src/ui/banner.c
+++ src/ui/banner.c
@@ -64,8 +64,8 @@ struct Impl_Banner {
 iDefineTypeConstruction(Banner)
 
 #define itemGap_Banner_     (3 * gap_UI)
-#define itemVPad_Banner_    (3 * gap_UI)
-#define itemHPad_Banner_    (4 * gap_UI)
+#define itemVPad_Banner_    (2 * gap_UI)
+#define itemHPad_Banner_    (3 * gap_UI)
 #define bottomPad_Banner_   (4 * gap_UI)
 
 static void updateHeight_Banner_(iBanner *d) {
