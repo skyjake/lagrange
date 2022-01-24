8f22d63a3476f654e33c00a0e3087b50793fa940
Jaakko Keränen <jaakko.keranen@iki.fi>
2022-01-24 12:39:20 +0200

    DocumentWidget: Cancelling an input query
    This has been broken for a while...

--- src/ui/documentwidget.c
+++ src/ui/documentwidget.c
@@ -4069,14 +4069,12 @@ static iBool handleCommand_DocumentWidget_(iDocumentWidget *d, const char *cmd) 
         return iTrue;
     }
     else if (equal_Command(cmd, "valueinput.cancelled") &&
-             equal_Rangecc(range_Command(cmd, "id"), "document.input.submit") && document_App() == d) {
+             equal_Rangecc(range_Command(cmd, "id"), "!document.input.submit") && document_App() == d) {
         postCommand_Root(get_Root(), "navigate.back");
         return iTrue;
     }
     else if (equalWidget_Command(cmd, w, "document.request.updated") &&
              id_GmRequest(d->request) == argU32Label_Command(cmd, "reqid")) {
-//        set_Block(&d->sourceContent, &lockResponse_GmRequest(d->request)->body);
-//        unlockResponse_GmRequest(d->request);
         if (document_App() == d) {
             updateFetchProgress_DocumentWidget_(d);
         }
