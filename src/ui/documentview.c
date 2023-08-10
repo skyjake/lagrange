1b9424e2d793bc602f70f54f433e4d64c4f8d75a
Jaakko Keränen <jaakko.keranen@iki.fi>
2023-08-10 13:12:17 +0300

    DocumentView: Fixed a clipping issue

--- src/ui/documentview.c
+++ src/ui/documentview.c
@@ -1653,7 +1653,7 @@ void draw_DocumentView(const iDocumentView *d, int horizOffset) {
     }
     else {
         iRect boundsWithOffset = moved_Rect(bounds, init_I2(horizOffset, 0));
-        fillRect_Paint(&ctx.paint, boundsWithOffset, uiBackground_ColorId);
+        fillRect_Paint(&ctx.paint, intersect_Rect(boundsWithOffset, clipBounds), uiBackground_ColorId);
         drawLogo_MainWindow(get_MainWindow(), boundsWithOffset);
     }
     /* Fill the top safe area above the view, if there is one. */
